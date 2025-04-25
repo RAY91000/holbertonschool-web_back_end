236b4cc6b390d5055175369dcfe0c501eab4b46f
ray91000 <ray.dotropg@gmail.com>
2025-04-25 21:49:28 +0200

    pycodestyle for 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,9 +7,11 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """
-    type-annotated function sum_list which takes a list input_list of floats
-    as argument and returns their sum as a float.
-    Remark from Python 3.9+, can replace typing.List by just list[]
+    type-annotated function sum_list which takes a list
+    input_list of floatsas argument and
+    returns their sum as a float.
+    Remark from Python 3.9+, can replace typing.
+    List by just list[]
     """
     sum: float = 0.0
     for number in input_list:
