e1124ebf07f521ca260a7dcc3597e16c4c27d102
ray91000 <ray.dotropg@gmail.com>
2025-04-25 16:28:18 +0200

    pycodestyle for 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """
-This module serves as basic example for variable annotation for type float
+This module serves as basic example for variable annotation
+for type float
 """
 
 
 def add(a: float, b: float) -> float:
     """
-    type-annotated function add that takes a float a and a float b as arguments
-    and returns their sum as a float.
+    type-annotated function add that takes a float a and a float b as
+    argumentsand returns their sum as a float.
     """
     return a + b
