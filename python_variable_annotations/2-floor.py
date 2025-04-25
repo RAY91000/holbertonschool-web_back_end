a4bba79ab7051e53a7ed9003c9605ead1f90f6ae
ray91000 <ray.dotropg@gmail.com>
2025-04-25 21:43:23 +0200

    pycodestyle

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-This module serves as basic example for variable annotation
-for type float and int
+This module serves as basic example for variable
+annotation for type float and int
 """
 
 
 def floor(n: float) -> int:
     """
-    type-annotated function floor which takes a float n as argument
-    and returns the floor of the float.
+    type-annotated function floor which takes a float
+    n as argumentand returns the floor of the float.
     """
     return int(n)
