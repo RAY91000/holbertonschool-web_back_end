a58c1f0a495d32b2d6cf6511bd77748cf56618c0
ray91000 <ray.dotropg@gmail.com>
2025-04-28 23:21:26 +0200

    debugging task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,10 @@ from typing import AsyncGenerator
 import asyncio
 import random
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Loops 10 times asynchronously, yielding random number after 1 second each time"""
 
-    for _ in range(10):
-        await asyncio.sleep(1)  # D'abord attendre 1 seconde
-        yield random.uniform(0, 10)  # Puis envoyer un nombre aléatoire
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ Loops 10 times asyncronously, yields random num """
+
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
