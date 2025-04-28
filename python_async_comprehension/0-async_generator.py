91cc141215e2f8d30dd4ed01f44bb2e1786ba072
ray91000 <ray.dotropg@gmail.com>
2025-04-28 23:19:16 +0200

    coorecting tasko 0 by imported asyncgenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+""" Coroutine with async """
 
+from typing import AsyncGenerator
 import asyncio
 import random
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Loops 10 times asynchronously, yielding random number after 1 second each time"""
+
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        await asyncio.sleep(1)  # D'abord attendre 1 seconde
+        yield random.uniform(0, 10)  # Puis envoyer un nombre aléatoire
